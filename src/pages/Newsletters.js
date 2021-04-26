1bff9ba429a57f6b357e9b4603c26f880f72ef77
Alex Jederlinic <alex@jederock.com>
2021-04-26 09:05:26 -0400

    Is this working?

--- src/pages/Newsletters.js
+++ src/pages/Newsletters.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { AmplifySignOut } from '@aws-amplify/ui-react';
 import NewsletterGrid from '../components/NewsletterGrid'
 import Title from '../components/Title'
+import Nav from '../components/Nav'
 
 export default function Newsletters() {
   return (
     <div>
     <Title />
+    <Nav />
     <NewsletterGrid />
     <AmplifySignOut />
     </div>
